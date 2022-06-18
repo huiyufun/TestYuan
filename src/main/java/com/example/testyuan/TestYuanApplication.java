30a223f51e93174705c82c9210a7a40f20d4ceb0
huiyufun <1974558483@qq.com>
2022-06-18 17:41:02 +0800

    初始项目

---
+++ src/main/java/com/example/testyuan/TestYuanApplication.java
@@ -0,0 +1,13 @@
+package com.example.testyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestYuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestYuanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testyuan/TestYuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testyuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestYuanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
